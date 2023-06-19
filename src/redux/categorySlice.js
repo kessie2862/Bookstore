38d70abda06890d292f12c27d5a516276f8b4d04
kessie <kayp3477@gmail.com>
2023-06-19 13:48:23 +0000

    Add checkStatus reducer

--- src/redux/categorySlice.js
+++ src/redux/categorySlice.js
@@ -2,8 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const categorySlice = createSlice({
   name: 'categories',
-  initialState: 'Under construction',
-  reducers: {},
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+    checkStatus: (state) => state,
+  },
 });
 
+export const { checkStatus } = categorySlice.actions;
 export default categorySlice.reducer;
