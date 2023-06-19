b9e5766d3602b08a80faf7ea563859a1db785f69
kessie <kayp3477@gmail.com>
2023-06-19 10:44:54 +0000

    Define category slice

---
+++ src/redux/categorySlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categorySlice = createSlice({
+  name: 'categories',
+  initialState: 'Under construction',
+  reducers: {},
+});
+
+export default categorySlice.reducer;
