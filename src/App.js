01941b3acce287f6b743411cb30e74ecae74e788
kessie <kayp3477@gmail.com>
2023-06-19 10:40:11 +0000

    Wrap App with Provider component passing redux store as prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Books from './components/Books';
 import Navigation from './components/Navigation';
 import Categories from './components/Categories';
 
 const App = () => (
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
