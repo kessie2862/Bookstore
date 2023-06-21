db309fccd93d4ad9b27d9d2eb0f0c5e9c918008e
kessie <kayp3477@gmail.com>
2023-06-21 13:47:10 +0000

    Imported and used BookForm component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,33 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import BookForm from './BookForm';
 import BookItem from './BookItem';
 
-const BookList = ({ books, handleRemoveBook }) => (
+const Books = () => (
   <div>
-    {books.length === 0 ? (
-      <p>No books available</p>
-    ) : (
-      books.map((book) => (
-        <BookItem
-          key={book.item_id}
-          book={book}
-          handleRemoveBook={handleRemoveBook}
-        />
-      ))
-    )}
+    <BookItem />
+    <BookForm />
   </div>
 );
 
-BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      item_id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      category: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  handleRemoveBook: PropTypes.func.isRequired,
-};
-
-export default BookList;
+export default Books;
