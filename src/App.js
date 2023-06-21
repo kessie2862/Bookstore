b7a12a02236a219d7305aaab3011fc80d6fe7f0d
kessie <kayp3477@gmail.com>
2023-06-21 13:41:23 +0000

    Imported Books component from BookList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Books from './components/Books';
+import Books from './components/BookList';
 import Navigation from './components/Navigation';
 import Categories from './components/Categories';
 
