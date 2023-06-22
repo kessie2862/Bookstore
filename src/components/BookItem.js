90a778faac5004c0e45a69ad4307285d0b636d6e
kessie <kayp3477@gmail.com>
2023-06-22 15:12:47 +0000

    Add classNames to elements for styling

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBooks, removeBook } from '../redux/bookSlice';
+import '../styles/BookItem.css';
 
 const BookItem = () => {
   const dispatch = useDispatch();
@@ -22,38 +23,52 @@ const BookItem = () => {
     return null;
   }
 
+  const style = {
+    visibility: 'hidden',
+    height: 0,
+    width: 0,
+  };
+
   const renderBook = (itemId, book) => {
     const {
-      category, title, author, chapter,
+      category, title, author,
     } = book[0];
 
     return (
-      <div key={itemId}>
-        <div>
-          <p>{category}</p>
-          <h3>{title}</h3>
-          <p>{author}</p>
+      <div key={itemId} className="book-info">
+        <div className="Book-content">
           <div>
-            <button type="button">Comment</button>
-            <div>
-              <button
-                type="button"
-                onClick={() => dispatch(removeBook(itemId))}
-              >
-                Remove
-              </button>
-            </div>
-            <button type="button">Edit</button>
+            <p className="category">{category}</p>
+            <h3 className="title">{title}</h3>
+            <p className="author">{author}</p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="btns">Comment</button>
+            <button
+              className="btns btn-remove"
+              type="button"
+              onClick={() => dispatch(removeBook(itemId))}
+            >
+              Remove
+            </button>
+            <button type="button" className="btns">Edit</button>
           </div>
         </div>
-        <div>
-          <h4>75%</h4>
-          <div>Completed</div>
-        </div>
-        <div>
-          <h3>Current Chapter</h3>
-          <p>{chapter}</p>
-          <button type="button">Update Progress</button>
+        <div className="chapter-status">
+          <div className="status">
+            <div className="progress-bar">
+              <progress value="75" min="0" max="100" style={style}>75%</progress>
+            </div>
+            <div className="progress">
+              <p className="percentage">75%</p>
+              <p className="completed">Completed</p>
+            </div>
+          </div>
+          <div className="chapter">
+            <h3>CURRENT CHAPTER</h3>
+            <p>Chapter 17</p>
+            <button type="button">UPDATE PROGRESS</button>
+          </div>
         </div>
       </div>
     );
